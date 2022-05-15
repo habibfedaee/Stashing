cb4ca30c81fa3eef55ba1e006092f0f788935ecd
habibfedaee <habib.fedaee@gmail.com>
2022-05-15 09:05:16 -0700

    The red Audi car is ready with some basic features

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,13 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         // your codes here:
-        basicFeatures car = new basicFeatures();
-        
+        basicFeatures audi = new basicFeatures();
+        audi.Color="red";
+        audi.doors="4";
+        audi.engineCapacity="v8";
+        audi.headlights= "led";
+        audi.regNo="ca-habibi";
+
+
     }
 }
