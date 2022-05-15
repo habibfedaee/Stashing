76544ac954227bd7623fda72404d39b574731f98
habibfedaee <habib.fedaee@gmail.com>
2022-05-15 08:03:24 -0700

    the basic structure of the car is ready, working on advanced features

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        // your codes here:
+        basicFeatures car = new basicFeatures();
+        
+    }
+}
---
+++ src/com/company/basicFeatures.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class basicFeatures {
+    String engineCapacity;
+    String Color;
+    String doors;
+    String headlights;
+    String regNo;
+
+}
